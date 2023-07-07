853fbf562dcadb8b7555fd6e46fb1d8e99a52f9a
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-07-07 19:42:59 +0530

    Called all functions from smart contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import react, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { contractAddress, contractABI } from "./constant";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // general
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+  const [signerAddress, setSignerAddress] = useState(undefined);
+
+  // assets
+  const [assetIds, setAssetIds] = useState([]);
+  const [assets, setAssets] = useState([]);
+
+  // staking
+  const [showStakeModal, setShowStakeModal] = useState(false);
+  const [stakingLength, setStakingLength] = useState(undefined);
+  const [stakingPercent, setStakingPercent] = useState(undefined);
+  const [amount, setAmount] = useState(0);
+
+  // helpers
+  const toWei = (ether) => ethers.utils.parseEther(ether);
+  const toEther = (wei) => ethers.utils.formatEther(wei);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider);
+
+      const contract = await new ethers.Contract(contractAddress, contractABI);
+      setContract(contract);
+    };
+    onLoad();
+  }, []);
+
+  const isConnected = () => signer !== undefined;
+
+  const getSigner = async () => {
+    provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    return signer;
+  };
+
+  const getAssetIds = async (address, signer) => {
+    const assetIds = await contract
+      .connect(signer)
+      .getPositionIdsForAddress(address);
+    return assetIds;
+  };
+
+  const calcDaysRemaining = (unlockDate) => {
+    const timeNow = Date.now() / 1000;
+    const secondsRemaining = unlockDate - timeNow;
+    return Math.max((secondsRemaining / 60 / 60 / 24).toFixed(0), 0);
+  };
+
+  const getAssets = async (ids, signer) => {
+    const queriedAssets = await Promise.all(
+      ids.map((id) => contract.connect(signer).getPositionById(id))
+    );
+
+    queriedAssets.map(async (asset) => {
+      const parsedAsset = {
+        positionId: asset.positionId,
+        percentInterest: Number(asset.percentInterest) / 100,
+        daysRemaining: calcDaysRemaining(Number(asset.unlockDate)),
+        etherInterest: toEther(asset.weiInterest),
+        etherStaked: toEther(asset.weiStaked),
+        open: asset.open,
+      };
+
+      setAssets((prev) => [...prev, parsedAsset]);
+    });
+  };
+
+  const connectAndLoad = async () => {
+    const signer = await getSigner(provider);
+    setSigner(signer);
+
+    const signerAddress = await signer.getAddress();
+    setSignerAddress(signerAddress);
+
+    const assetIds = await getAssetIds(signerAddress, signer);
+    setAssetIds(assetIds);
+
+    getAssets(assetIds, signer);
+  };
+
+  const openStakingModal = (stakingLength, stakingPercent) => {
+    setShowStakeModal(true);
+    setStakingLength(stakingLength);
+    setStakingPercent(stakingPercent);
+  };
+
+  const stakeEther = () => {
+    const wei = toWei(amount);
+    const data = { value: wei };
+    contract.connect(signer).stakeEther(stakingLength, data);
+  };
+
+  const withdraw = (positionId) => {
+    contract.connect(signer).closePosition(positionId);
+  };
+
+  return <div className="App"></div>;
 }
 
 export default App;
